edade8dad10bb4c9081b4ec03b2144d63702d8a1
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-01 17:34:45 +0900

    factorybotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,12 @@
 FactoryBot.define do
+
   factory :user do
     name                  {Faker::Name.initials(number: 2)}
     family_name           {Gimei.last.kanji}
     first_name            {Gimei.first.kanji}
     family_name_kana      {Gimei.last.katakana}
     first_name_kana       {Gimei.first.katakana}
-    email                 {Faker::Internet.free_email}
+    email                 {Faker::Internet.unique.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     birthday              {Faker::Date.birthday}
