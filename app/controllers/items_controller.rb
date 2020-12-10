d1f805aea699db47f200a54b6b2bf45b5b78bfc8
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-10 14:49:35 +0900

    エラー表示＆テーブルへの保存確認

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,22 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def create
-  end
-
   def new
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+
+    if @item.valid?
+      @item.save
+      return redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :days_to_ships_id).merge(user_id: current_user.id)
   end
 end
