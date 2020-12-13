9bd35fcd0b08d7561bb0d46d4c66b5dc410e1f01
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-13 14:12:00 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -12,7 +12,7 @@ class Item < ApplicationRecord
     validates :image
     validates :name, length: { maximum: 40 }
     validates :explanation, length: { maximum: 1000 }
-    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than: 300, less_than: 9_999_999 }
+    validates :price, format: { with: /\A[0-9]+\z/ }, numericality: { greater_than: 299, less_than: 10_000_000 }
 
     with_options numericality: { other_than: 1 } do
       validates :category_id
