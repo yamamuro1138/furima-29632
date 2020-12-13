c359abb9a8e91a24577ab01490612756963b1b2f
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-13 11:53:17 +0900

    手数料.利益計算結果の表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -3,7 +3,11 @@ window.addEventListener('load', function(){
   const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
+  
+    const salesCommission = document.getElementById("add-tax-price")
+    salesCommission.innerHTML = (Math.floor(inputValue * 0.1));
+
+    const salesProfit = document.getElementById("profit")
+    salesProfit.innerHTML = (Math.floor(inputValue - salesCommission.innerHTML));
   })
-  const salesCommission = document.getElementById("add-tax-price")
-  const salesProfit = document.getElementById("profit")
 })
