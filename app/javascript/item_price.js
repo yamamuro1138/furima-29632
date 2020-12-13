c8f084b6b4ee8ec99df1d3318827c79d946b851f
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-13 11:02:34 +0900

    value属性を取得

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -2,7 +2,7 @@ window.addEventListener('load', function(){
 
   const priceInput = document.getElementById("item-price")
   priceInput.addEventListener("input", () => {
-    console.log("イベント発火");
+    const inputValue = priceInput.value;
   })
   const salesCommission = document.getElementById("add-tax-price")
   const salesProfit = document.getElementById("profit")
