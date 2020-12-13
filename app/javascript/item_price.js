576d14e466ba97335f6fcf0c47cb52b3527e611c
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-13 10:35:01 +0900

    jsファイル作成＆要素の取得

---
+++ app/javascript/item_price.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price")
+  const salesCommission = document.getElementById("add-tax-price")
+  const salesProfit = document.getElementById("profit")
+})
