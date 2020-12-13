45e650966f93ccb591dc7115dee2ec02b8b09a56
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-13 10:46:37 +0900

    イベント発火

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,6 +1,9 @@
 window.addEventListener('load', function(){
 
   const priceInput = document.getElementById("item-price")
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
   const salesCommission = document.getElementById("add-tax-price")
   const salesProfit = document.getElementById("profit")
 })
