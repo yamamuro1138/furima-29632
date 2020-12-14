8aaeeec8ebdd996f25fff1a24eeb9a9f61abb219
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-14 22:19:05 +0900

    コントローラにshowアクションを定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :days_to_ships_id).merge(user_id: current_user.id)
   end
