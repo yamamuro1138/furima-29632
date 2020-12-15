3946405c7e86804939449685c29c5dc9fd3ad545
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-15 22:40:17 +0900

    updateアクションの定義＆編集後の遷移＆エラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,6 +28,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :price, :category_id, :status_id, :delivery_fee_id, :shipping_area_id, :days_to_ships_id).merge(user_id: current_user.id)
   end
