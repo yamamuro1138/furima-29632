814425af16a81ecc661ddae3a5560a258ee41ff3
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-20 15:03:53 +0900

    FactoryBotの修正

--- spec/factories/purchase_deliveries.rb
+++ spec/factories/purchase_deliveries.rb
@@ -8,8 +8,6 @@ FactoryBot.define do
     building_name      {'東京ハイツ'}
     user_id            {1}
     item_id            {1}
-
-    association :user
-    association :item
+    token              {"tok_abcdefghijk00000000000000000"}
   end
 end
