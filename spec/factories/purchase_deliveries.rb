ffbed05386d5fb6f98b425c12d3d395be395899f
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-20 14:07:17 +0900

    FactoryBot作成

--- spec/factories/purchase_deliveries.rb
+++ spec/factories/purchase_deliveries.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :purchase_delivery do
-    
+    postal_code        {'123-4567'}
+    shipping_area_id   {2}
+    municipalities     {'東京都'}
+    address            {'青山1-1-1'}
+    phone_number       {'01234567890'}
+    building_name      {'東京ハイツ'}
+    user_id            {1}
+    item_id            {1}
+
+    association :user
+    association :item
   end
 end
