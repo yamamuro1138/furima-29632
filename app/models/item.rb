34f81498e0cf6900bf897859150c1ce99fef9250
yamamurodaisuke <daisuke0308@icloud.com>
2020-12-24 20:00:14 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to_active_hash :days_to_ships
   belongs_to :user
   has_one_attached :image
+  has_one :purchase
   # バリデーションの設定
   with_options presence: true do
     validates :image
